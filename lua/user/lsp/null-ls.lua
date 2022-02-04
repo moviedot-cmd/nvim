2e9e38e2b8106dac5c0b6643d74ef9906a59512b
mateooviedo <mateooviedotorres@gmail.com>
2022-02-03 22:09:28 -0500

    Config format and indentline

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-  return
+	return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -8,13 +8,20 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
-null_ls.setup {
-  debug = false,
-  sources = {
-    formatting.prettier.with { extra_args = { "--single-quote", "--jsx-single-quote" } },
-    formatting.black.with { extra_args = { "--fast" } },
-    -- formatting.yapf,
-    formatting.stylua,
-    diagnostics.flake8,
-  },
-}
+null_ls.setup({
+	debug = false,
+	sources = {
+		formatting.prettier.with({
+			extra_args = {
+				"--single-quote",
+				"--jsx-single-quote",
+				"--tab-widths",
+				"--use-tabs",
+			},
+		}),
+		formatting.black.with({ extra_args = { "--fast" } }),
+		-- formatting.yapf,
+		formatting.stylua,
+		diagnostics.flake8,
+	},
+})
