b496f38ab21632907fdbdabc241c91667fef65cb
mateooviedo <mateooviedotorres@gmail.com>
2024-06-06 07:03:44 -0500

    feat: update mappings

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,16 +1,11 @@
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
--- Barbar mappings
--- Move to previous/next
-map("n", "<tab>", "<Cmd>BufferNext<CR>", opts)
-map("n", "<S-tab>", "<Cmd>BufferPrevious<CR>", opts)
-
 -- Buffers
 map("n", "tk", ":blast<enter>", { noremap = false })
 map("n", "tj", ":bfirst<enter>", { noremap = false })
-map("n", "th", ":bprev<enter>", { noremap = false })
-map("n", "tl", ":bnext<enter>", { noremap = false })
+map("n", "<S-tab>", ":bprev<enter>", { noremap = false })
+map("n", "<tab>", ":bnext<enter>", { noremap = false })
 map("n", "td", ":bdelete<enter>", { noremap = false })
 
 -- Move between windows
