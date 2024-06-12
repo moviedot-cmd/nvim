93fa8f2261304470d2b24a1df58b7ea382bad87a
mateooviedo <mateooviedotorres@gmail.com>
2024-06-12 00:58:08 -0500

    fix: ignore errors when the file not exits

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -9,6 +9,7 @@ return {
 			typescript = { "eslint" },
 			javascriptreact = { "eslint" },
 			typescriptreact = { "eslint" },
+			css = { "stylelint" },
 		}
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
@@ -16,12 +17,12 @@ return {
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 			group = lint_augroup,
 			callback = function()
-				lint.try_lint()
+				lint.try_lint(nil, { ignore_errors = true })
 			end,
 		})
 
 		vim.keymap.set("n", "<leader>lL", function()
-			lint.try_lint()
+			lint.try_lint(nil, { ignore_errors = true })
 		end, { desc = "Trigger linting for current file" })
 	end,
 }
